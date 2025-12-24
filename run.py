0159ce17efd6fa1d61b305a059a2903355575d9b
MaxAvas <116870378+Kensaroshi@users.noreply.github.com>
2025-12-24 19:44:43 +0700

    Add run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-Add main runner
+#!/usr/bin/env python3
+from banner import show_banner
+from menu import main_menu
+
+def clear():
+    print("\033c", end="")
+
+clear()
+show_banner()
+main_menu()
