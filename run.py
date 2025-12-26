1207e8fcbe94458ad27373497dad20c0c13b4ff3
MaxAvas <116870378+Kensaroshi@users.noreply.github.com>
2025-12-26 18:15:32 +0700

    run.py

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 from banner import show_banner
 from menu import main_menu
-
-def clear():
-    print("\033c", end="")
+from core.effects import clear
 
 clear()
 show_banner()
